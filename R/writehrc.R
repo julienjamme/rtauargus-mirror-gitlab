0d7a500390d373d569ded87ecc5d3f3dc9025185
Beroud Félix <EA7Q7G@ad.insee.intra>
2022-07-08 18:07:21 +0200

    Ajouts divers : vérifications au lancement, formattage, tri, option rev et option sort_table

--- R/writehrc.R
+++ R/writehrc.R
@@ -1,87 +1,160 @@
-#' Ecrit une hiérarchie .hrc à partir d'une table de correspondance
+#' .hrc writing
+#'
 #' Creates a .hrc hierarchy from a correspondence table
+#' Ecrit une hiérarchie .hrc à partir d'une table de correspondance
 #'
-#' @param corr_table Data frame ; table de correspondance, du plus agrégé au
+#' @param corr_table Correspondence table, from most aggregated to most detailed
+#' \cr
+#' Data frame ; table de correspondance, du plus agrégé au
 #' plus fin
-#' Correspondence table, from most aggregated to most detailed
-#' @param output_name Nom du fichier en sortie (sans extension), ; par défaut,
+#' @param output_name character string. Name for the output file (with no
+#' extension) ; default is set to the same name as the correspondence table
+#' \cr
+#' Nom du fichier en sortie (sans extension), ; par défaut,
 #' identique au nom de la table de correspondance
-#' Name for the output file (with no extension) ; default is set to the same
-#' name as the correspondence table
-#' @param dir_name Nom du répertoire dans lequel écrire le fichier hrc
-#' Directory name for the hrc file
-#' @param hier_lead_string Caractère unique repérant le niveau de profondeur
-#' dans le .hrc
-#' (Single) character indicating the hierarchy depth in the .hrc file
+#' @param dir_name character string. Directory name for the hrc file
+#' \cr
+#' Nom du répertoire dans lequel écrire le fichier hrc
+#' @param sort_table boolean. If TRUE, table will be sorted beforehand.
+#' Recommended.\cr
+#' Si TRUE, la table sera triée avant traitement. Recommandé.
+#' @param rev boolean. If TRUE, column order is reversed.\cr
+#' Si TRUE, inverse l'ordre des colonnes.
+#' @param hier_lead_string #' character. (Single) character indicating the
+#' hierarchy depth in the .hrc file \cr
+#' Caractère unique repérant le niveau de profondeur dans le .hrc
 #'
-#' @details Ecrit un fichier de hiérarchie .hrc lisible par $\tau$-Argus à
+#' @details #' Creates a .hrc hierarchy file adapted to tau-Argus from a correspondence
+#' table. The table must be written :
+#' - from the most aggregated level to the most detailed
+#' - well-nested: any fine level must be nested into a unique higher level
+#' \cr
+#' Ecrit un fichier de hiérarchie .hrc lisible par tau-Argus à
 #' partir d'une table de corrrespondance la décrivant complètement.
 #' Il est nécessaire que la table de correspondance soit :
 #' - écrite du niveau le plus agrégé au plus fin
 #' - bien emboîtée : un niveau fin appartient à un unique niveau supérieur
-#' Creates a .hrc hierarchy file adapted to $\tau$-Argus from a correspondence
-#' table. The table must be written :
-#' - from the most aggregated level to the most detailed
-#' - well-nested: any fine level must be nested into a unique higher level
 #'
-#' @return Pas de sortie R.
-#' No R output.
+#' @section Details about correspondence table & .hrc
+#' Hierarchy files read by tau-Argus are expected to follow a strict pattern.
+#' If there is a problem using the .hrc file (for instance, error making
+#' totals), please check the following : \cr
+#'
+#' 1. Are columns ordered from most aggregated to most detailed ?
+#' If they are in reverse order, you may want to use rev = TRUE or simply order
+#' the correspondence table before trying again.\cr
+#'
+#' 2. Is the hierarchy well-nested ? Fine levels must systematically be nested
+#' into unique higher levels.
+#' If not, you will have to treat separately the different hierarchies, and then
+#' merge masks until stable.\cr
+#'
+#' 3.
+#'
+#' @return NULL
 #'
 #' @export
 #'
 #' @examples
-#' corr_a88 = read.csv("data/correspondance_a88.csv",
-#'   quote = "\"",colClasses = c(a88 = "character"))
-#' write.hrc(corr_a88, "example_hrc")
+#' astral_hrc <- data.frame(
+#' type = c("planet", "planet", "star", "star", "star", "other", "other"),
+#' details = c("telluric", "gasgiant", "basic", "whitedwarf", "browndwarf", "blackhole", "neutronstar" )
+#' )
+#' write_hrc2(astral_hrc)
 #'
 
-write.hrc <- function(corr_table,
-                            output_name = NULL,
-                            dir_name = NULL,
-                            hier_lead_string = "@"){
+write_hrc2 <- function(corr_table,
+                       output_name = NULL,
+                       dir_name = NULL,
+                       sort_table = TRUE,
+                       rev = FALSE,
+                       hier_lead_string = getOption("rtauargus.hierleadstring")
+){
+
+  d = dim.data.frame(corr_table)
 
   #### Basic verifications & formatting
 
+  # Fill in output name
   if (is.null(output_name)) {
     givenfilename <- deparse(substitute(corr_table))
     output_name <- givenfilename
   }
 
-  if (!is.data.frame(corr_table)) warning("corr_table should be a data frame.")
-
-  if(nchar(hier_lead_string) != 1){
-    message("hier_lead_string has to be a string of only one character.")
-    return(invisible(NULL))
-  }
-
-  corr_table <- as.data.frame(apply(corr_table, 2, FUN = as.character))
-
+  # Default directory
   dir_name <- if(is.null(dir_name)) getwd() else dir_name
 
+  # Reverse column order if asked
+  if (rev) corr_table <- rev(corr_table)
 
-  ####
+  # Make corr_table a data frame, or raise error
+  corr_table <- tryCatch(
+    {
+      as.data.frame(corr_table)
+    },
+    error = function(msg){
+      stop("Cannot coerce corr_table to a data frame")
+      print(msg)
+    }
+  )
+
+  # Check hier_lead_string
+  if(nchar(hier_lead_string) != 1){
+    stop("hier_lead_string should be 1 single character")
+  }
+
+  # Warn about presence of NAs
+  if (sum(is.na(corr_table))>0){
+    warning("Missing values in correspondence table will be ignored. If unintended, this can cause errors when using the .hrc file with tau-Argus.")
+  }
+  # (Todo : lister cas de NA non gênantes et bloquer les autres)
+
+  # Try to detect a problem with detailed column
+  if (sum(duplicated(corr_table[,d[2]]))>0) {
+  warning("There are duplicates in the most detailed level (last column).
+          Please check table")
+  }
+
+  # Check if all columns are character
+  suspects <- NULL
+  for (col in 1:d[2]){
+    if (!is.character(corr_table[,col])) {
+      suspects <- c(suspects, col)
+    }
+  }
+  if (!is.null(suspects))  message("Note : the following columns are not of character type : ", colnames(corr_table)[suspects], ". There may be an issue reading the table.")
+
+  #### Creating the hrc file
+
+  # 0. Sort the correspondence table
+  if (sort_table){
+    for (j in 1:d[2]){
+      corr_table <- corr_table[
+        order(corr_table[,d[2]-j+1])
+        ,]
+    }
+  }
 
   # 1. Create a 1-step lagged version of the correspondence table
-  d = dim.data.frame(corr_table)
   corr_table_decale <- rbind(
     corr_table[d[1]+1,],
     corr_table[1:(d[1]-1),]
-    )
-
-  # 2. Add a fitting number of hier_lead_string to all
-  # & erase cells identical to the one just before them
+  )
+  # (same dimensions as corr_table ; any line i is equal to line (i-1) of the
+  # original table, except for 1rst line which is just NAs)
 
   compare <- corr_table == corr_table_decale #<-- cells identical to their upper
   # neighbour
   missing <- is.na(corr_table)
 
+  # 2. Add a fitting number of hier_lead_string to all
+  # & erase cells identical to the one just before them
+
   for (colonne in 1:d[2]) {
-    corr_table[,colonne] <- apply(
-      as.data.frame(corr_table[,colonne]),
-      1:2,
-      FUN = arobase,
-      num_col = colonne,
-      hier_lead_string = hier_lead_string)
+    corr_table[,colonne] <- paste0(
+      paste0(rep(hier_lead_string,colonne -1), collapse = ""),
+      corr_table[,colonne],
+      "\n")
   }
   corr_table[compare] <- ""
   corr_table[missing] <- ""
@@ -98,16 +171,4 @@ write.hrc <- function(corr_table,
               col.names = FALSE,
               sep="",
               eol = "")
-}
-
-
-arobase <- function(case,num_colonne, hier_lead_string) {
-  # Adds to a given cell :
-  # - as many @ as necessary given the hierarchical depth
-  # - a line break for the final writing of the file
-  if (num_colonne == 1) {paste(case,"\n", collapse = "", sep = "")
-    } else {
-  paste(c(rep(hier_lead_string,num_colonne-1),case,"\n"),
-          collapse = "", sep = "")
-    }
 }
